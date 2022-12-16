6fb3d00790476e683829d5f498e17178f77d3d12
LobodaEU <114651106+LobodaEU@users.noreply.github.com>
2022-12-16 10:43:21 +0300

    v 1.4 spiner + copi

--- app/src/main/java/com/loboda/generator/MainActivity.java
+++ app/src/main/java/com/loboda/generator/MainActivity.java
@@ -3,12 +3,16 @@ package com.loboda.generator;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Random;
@@ -16,6 +20,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 Button button;
 Button button2;
+Button button3;
 TextView textView;
 TextInputEditText textInputEditText;
 Spinner spinner;
@@ -30,6 +35,7 @@ Spinner spinner2;
 
         button = (Button) findViewById(R.id.button);
         button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
         textView = (TextView)  findViewById(R.id.textView);
         textInputEditText = (TextInputEditText)  findViewById(R.id.textInputEditText);
         spinner = (Spinner) findViewById(R.id.spinner) ;
@@ -38,8 +44,7 @@ Spinner spinner2;
         String[] parikmaherskaya = new String[]{"#прическа","#укладка","#барбершоп","#барбер", "#стрижка"};
         String[] prikoli = new String[]{"#music","#genre","#song","#songs","#TagsForLikes","#melody","#hiphop","#rnb","#pop","#love","#rap","#dubstep","#instagood","#beat","#beats","#jam","#myjam","#party","#partymusic","#newsong","#lovethissong","#remix","#favoritesong","#bestsong","#photooftheday","#bumpin","#repeat","#listentothis","#goodmusic","#instamusic","#movies","#theatre","#video","#TagsForLikes","#movie","#film","#films","#videos","#actor","#actress","#cinema","#dvd","#amc","#instamovies","#star","#moviestar","#photooftheday","#hollywood","#goodmovie","#instagood","#flick","#flicks","#instaflick","#instaflicks","#videogames","#games","#gamer","#TagsForLikes","#gaming","#instagaming","#instagamer","#playinggames","#online","#photooftheday","#onlinegaming","#videogameaddict","#instagame","#instagood","#gamestagram","#gamerguy","#gamergirl","#gamin","#video","#game","#igaddict","#winning","#play","#playing","#актер","#актриса","#беспризорник","#бессмертных","#биения","#бить","#варенье","#видео","#видео","#видео","#видеодиск","#видеоигры","#восстание","#геймер","#голливудский","#дабстеп","#жанр","#звезда","#игра","#игра","#играть","#игрище","#игровой","#интернете","#кино","#кино","#любовь","#мелодия","#музыка","#останавливает","#песни","#песня","#победивший","#повторять","#поп","#Последователи","#ремикс","#рит","#рэп","#сторона","#театр","#фильм","#фильмы","#хип-хоп","#цифровой","#юмор","#смешноевидео","#прикол","#прикольноевидео","#видос","#видеоролик","#шутка","#пранк","#розыгрыш","#арена","#цирк","#искуство","#театр","#спектакль","#микс","#комикс","#поржать","#смех"};
         String[] novosti = new String[]{"#новости","#свежиеновости","#информация","#информациякразмышлению", "#сегодня"};
-        String[] svoi_massiv = new String[]{};
-        String svoi_text = new String();
+        String[] svoy_spisok = new String[]{};
 
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -68,12 +73,14 @@ Spinner spinner2;
                                 else {
                                     ostatok = 0;
                                 }
+                                break;
                             case "20":
                                 if (svoi_massiv.length < 20) {
                                     ostatok = (20 -  svoi_massiv.length);
                                 }
                                 else {
                                     ostatok = 0;}
+                                break;
 
                             case "30" :
                                 if (svoi_massiv.length < 30) {
@@ -81,21 +88,23 @@ Spinner spinner2;
                                 }
                                 else {
                                     ostatok = 0;}
+                                break;
                             case "40":
                                 if (svoi_massiv.length < 40) {
                                     ostatok = (40 -  svoi_massiv.length);
                                 }
                                 else {
                                     ostatok = 0;}
-
+                                break;
                         }
+                        ostatok = ostatok + 2;
                         switch(spinner.getSelectedItem().toString()){
                             case "Ноготочки":
                                 if (nogti.length > 0) {
                                     for (int j = 0; j < ostatok; j++) {
                                         int rnd = new Random().nextInt(nogti.length + 1);
                                         if (rnd  > 0 ){rnd = (rnd - 1); }
-                                        hashteg = hashteg + nogti[rnd];
+                                        hashteg = hashteg +  nogti[rnd];
                                     }
                                 }
                                 break;
@@ -120,23 +129,69 @@ Spinner spinner2;
 
                             case "Новости":
                                 if (novosti.length > 0) {
-                                for (int j = 0; j < ostatok; j++) {
-                                    hashteg = hashteg + novosti[new Random().nextInt(novosti.length + 1)] ;}
+                                    for (int j = 0; j < ostatok; j++) {
+                                        int rnd = new Random().nextInt(novosti.length + 1);
+                                        if (rnd  > 0 ){rnd = (rnd - 1); }
+                                        hashteg = hashteg + novosti[rnd];
+                                    }
+                                }
+                                break;
+                            case "Свой список":
+                                if (svoy_spisok.length > 0) {
+                                    for (int j = 0; j < ostatok; j++) {
+                                        int rnd = new Random().nextInt(svoy_spisok.length + 1);
+                                        if (rnd  > 0 ){rnd = (rnd - 1); }
+                                        hashteg = hashteg + svoy_spisok[rnd];
+                                    }
                                 }
                                 break;
 
                         }
-                        textView.setText("Кнопка 1. генератор");
                         textView.setText(hashteg);
+
                         break;
                     case R.id.button2:
-                        textView.setText("Кнопка 2. Копирование");
+                        //textView.setText("Кнопка 2. Копирование");
+                        ClipboardManager myClipboard;
+                        myClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+                        ClipData myClip;
+                        myClip = ClipData.newPlainText("text", textView.getText().toString());
+                        myClipboard.setPrimaryClip(myClip);
+                        break;
+                    case R.id.button3:
+                        textView.setText("Кнопка 3. Save");
                         break;
 
                 }
 
             }
             };
+        spinner.setOnItemSelectedListener(new  AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView , int position , long id)
+            {
+                switch(spinner.getSelectedItem().toString()) {
+                    case "Свой список":
+                        textInputEditText.setHint(getResources().getString(R.string.TextView2));
+                        button3.setVisibility(Button.VISIBLE);
+                        textView.setText("");
+                        break;
+                    default:
+                        textInputEditText.setHint(getResources().getString(R.string.TextView1));
+                        button3.setVisibility(Button.GONE);
+                        textView.setText("");
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+
+        });
         button.setOnClickListener(onClickListener);
         button2.setOnClickListener(onClickListener);
         }
